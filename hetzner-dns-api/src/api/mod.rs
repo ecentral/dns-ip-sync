7004eb538e50dfda9f65dd719b73684443494402
Göran Bodenschatz <g.bodenschatz@ecentral.de>
2021-10-14 14:07:13 +0200

    Adapt hetzner_client structure form the cloudflare on

--- hetzner-dns-api/src/api/mod.rs
+++ hetzner-dns-api/src/api/mod.rs
@@ -5,8 +5,15 @@ use std::env;
 pub mod records;
 pub mod zones;
 
-pub fn hetzner_client(url: &str, method: Method) -> RequestBuilder {
+pub fn hetzner_client(path: &str, method: Method) -> RequestBuilder {
     let token = env::var("HETZNER_TOKEN").unwrap();
+
+    #[cfg(not(test))]
+        let url = "https://dns.hetzner.com/api/v1";
+    #[cfg(test)]
+        let url = &mockito::server_url();
+
+    let url = format!("{}/{}", url, path);
     reqwest::Client::new()
         .request(method, url)
         .header("Auth-API-Token", token.as_str())
--- hetzner-dns-api/src/api/zones.rs
+++ hetzner-dns-api/src/api/zones.rs
@@ -28,7 +28,7 @@ pub struct Zones {
 }
 
 pub async fn get_zones(name: Option<&str>) -> Result<Zones, Box<dyn std::error::Error>> {
-    let mut client = hetzner_client("https://dns.hetzner.com/api/v1/zones", Method::GET);
+    let mut client = hetzner_client("zones", Method::GET);
     if name.is_some() {
         client = client.query(&[("name", name.unwrap())]);
     }
